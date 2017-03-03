cc609b17988b722c51cfbfe231a82875b2b98f0c
Freaking Udemy App 2 <you@yourdomain.com>
2017-03-03 15:34:42 -0600

    commit before adding segmented importance function

---
+++ DOIT/Task.swift
@@ -0,0 +1,9 @@
+//
+//  Task.swift
+//  DOIT
+//
+//  Created by Jake Wojtas on 2/25/17.
+//  Copyright © 2017 Jake Wojtas. All rights reserved.
+//
+
+import Foundation
---
+++ DOIT/ToDoViewController.swift
@@ -0,0 +1,79 @@
+//
+//  ToDoViewController.swift
+//  DOIT
+//
+//  Created by Jake Wojtas on 2/25/17.
+//  Copyright © 2017 Jake Wojtas. All rights reserved.
+//  TO DO LIST
+//  Set importance on a ! !! !!! scale like Apple Reminders
+
+import UIKit
+
+class ToDoViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+
+    
+    @IBOutlet weak var tableView: UITableView!
+    
+    var tasks : [Task] = []
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // Do any additional setup after loading the view, typically from a nib.
+        
+        tasks = makeTasks()
+        
+        
+        tableView.dataSource = self
+        tableView.delegate = self
+    }
+
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return tasks.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = UITableViewCell()
+        let task = tasks[indexPath.row]
+        cell.textLabel?.text = task.name
+        if task.importance {
+            cell.textLabel?.text = "❗️\(task.name)"
+        } else {
+            cell.textLabel?.text = task.name
+            
+        }
+        return cell
+    }
+    
+    func makeTasks() -> [Task] {
+        let task1 = Task()
+        task1.name = "Walk the Dog"
+        task1.importance = false
+        
+        let task2 = Task()
+        task2.name = "FOLLOW YOUR DREAMS"
+        task2.importance = true
+        
+        let task3 = Task()
+        task3.name = "Buy textbooks"
+        task3.importance = false
+        
+        return [task1, task2, task3]
+    }
+    
+    @IBAction func plusTapped(_ sender: Any) {
+        performSegue(withIdentifier: "addSegue", sender: nil)
+    }
+    
+    
+    
+    
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
